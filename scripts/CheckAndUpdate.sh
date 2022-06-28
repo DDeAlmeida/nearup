5e65e12ccc801c8988c856e0d200057335fb4c9e
David <52310650+DDeAlmeida@users.noreply.github.com>
2022-06-28 11:06:34 +0200

    Update CheckAndUpdate.sh

--- scripts/CheckAndUpdate.sh
+++ scripts/CheckAndUpdate.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-curl -sL https://api.github.com/repos/near/nearcore/releases/latest | \
-          jq -r ".tag_name" > nearup/near-nearcore-latest.txt
+curl -sL https://api.github.com/repos/near/nearcore/releases/latest | jq -r ".tag_name" > near-nearcore-latest.txt
